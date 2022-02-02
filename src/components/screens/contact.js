8f97f707fde33a07b3842d90c37f7aab099a0eb0
lesly <leslyrevenge@gmail.com>
2022-02-02 13:43:44 -0500

    removed contact

--- src/components/screens/contact.js
+++ src/components/screens/contact.js
@@ -1,17 +1,5 @@
 import React from "react";
-import PageHelmet from "../common/Helmet";
-import Header from "../items/Header";
 
 export default function Contact() {
-  return (
-    <React.Fragment>
-      <PageHelmet pageTitle="Contact" />
-
-      <Header
-        headertransparent="header--transparent"
-        colorblack="color--black"
-        logoname="logo.png"
-      />
-    </React.Fragment>
-  );
+  return <div>Contact</div>;
 }
